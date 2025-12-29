417b9b3a33df64ba5580f1768265b282aa1fcb2b
shakirakahar31-design <shakirakahar31@gmail.com>
2025-12-30 00:50:48 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleVideoDetails() {
+    var x = document.getElementById("videoDetails");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
